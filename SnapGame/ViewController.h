7941210e1c85208d4d8ddf4bb6fbd6fb431ee59d
Juhnowski <juhnowski@gmail.com>
2018-06-03 03:15:00 +0300

    Initial Commit

---
+++ SnapGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapGame
+//
+//  Created by Илья Юхновский on 03.06.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
