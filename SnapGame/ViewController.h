5b308ba7212cf92a2d623266b4d16ecf94db52dc
Juhnowski <juhnowski@gmail.com>
2018-06-03 04:36:47 +0300

    first commit

--- SnapGame/ViewController.h
+++ SnapGame/ViewController.h
@@ -8,8 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    int timerInt;
+    
+    NSTimer *imageUpdate;
+    int scoreInt;
+}
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startOutlet;
+@property (weak, nonatomic) IBOutlet UIButton *snapOutlet;
+
+- (IBAction)startGame:(id)sender;
+- (IBAction)snap:(id)sender;
 
 @end
 
--- SnapGame/ViewController.m
+++ SnapGame/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    scoreInt = 0;
+    timerInt = 20;
+    self.snapOutlet.enabled = NO;
 }
 
 
@@ -25,5 +27,94 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)startGame:(id)sender {
+    if(scoreInt == 0){
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerUpdate) userInfo:nil repeats:YES];
+        self.snapOutlet.enabled = YES;
+        self.startOutlet.enabled = NO;
+    }
+    
+    if (timerInt == 0){
+        timerInt = 20;
+        scoreInt = 0;
+        
+        self.timeLabel.text = [NSString stringWithFormat:@"%i", timerInt];
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+        
+        [self.startOutlet setTitle:@"Start Game" forState:UIControlStateNormal];
+    }
+}
+
+-(void)timerUpdate {
+    timerInt -= 1;
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timerInt];
+    
+    int randomeOne = arc4random() % 6;
+    switch (randomeOne) {
+        case 0:
+            self.imageView1.image = [UIImage imageNamed:@"snap1.jpg"];
+            break;
+        case 1:
+            self.imageView1.image = [UIImage imageNamed:@"snap2.jpg"];
+            break;
+        case 2:
+            self.imageView1.image = [UIImage imageNamed:@"snap3.jpg"];
+            break;
+        case 3:
+            self.imageView1.image = [UIImage imageNamed:@"snap4.jpg"];
+            break;
+        case 4:
+            self.imageView1.image = [UIImage imageNamed:@"snap5.jpg"];
+            break;
+        case 5:
+            self.imageView1.image = [UIImage imageNamed:@"snap6.jpg"];
+            break;
+        default:
+            break;
+    }
+    
+    int randomeTwo = arc4random() % 6;
+    switch (randomeTwo) {
+        case 0:
+            self.imageView2.image = [UIImage imageNamed:@"snap1.jpg"];
+            break;
+        case 1:
+            self.imageView2.image = [UIImage imageNamed:@"snap2.jpg"];
+            break;
+        case 2:
+            self.imageView2.image = [UIImage imageNamed:@"snap3.jpg"];
+            break;
+        case 3:
+            self.imageView2.image = [UIImage imageNamed:@"snap4.jpg"];
+            break;
+        case 4:
+            self.imageView2.image = [UIImage imageNamed:@"snap5.jpg"];
+            break;
+        case 5:
+            self.imageView2.image = [UIImage imageNamed:@"snap6.jpg"];
+            break;
+        default:
+            break;
+    }
+    
+    if(timerInt == 0) {
+        [timer invalidate];
+        
+        self.snapOutlet.enabled = NO;
+        self.startOutlet.enabled = YES;
+        
+        [self.startOutlet setTitle:@"Restart Game" forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)snap:(id)sender {
+    if([self.imageView1.image isEqual:self.imageView2.image]) {
+        scoreInt += 1;
+    } else {
+        scoreInt -= 1;
+    }
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+}
 
 @end
